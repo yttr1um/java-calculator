58d78557f51ad873afade8b37bcffe93672c2c8f
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-08-31 08:26:55 +0400

    Added display panel

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,7 +8,14 @@ public class Calculator {
     int windowWidth = 360;
     int windowHeight = 540;
 
+    Color customLightGray = new Color(212, 212, 210);
+    Color customDarkGray = new Color(80, 80, 80);
+    Color customBlack = new Color(28, 28, 28);
+    Color customOrange = new Color(255, 149, 0);
+
     JFrame frame = new JFrame("calculator");
+    JLabel displayLabel = new JLabel();
+    JPanel displayPanel = new JPanel();
 
     public Calculator() {
         frame.setVisible(true);
@@ -17,5 +24,16 @@ public class Calculator {
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
+
+        displayLabel.setBackground(customBlack);
+        displayLabel.setForeground(Color.white);
+        displayLabel.setFont(new Font("Arial", Font.PLAIN, 80));
+        displayLabel.setHorizontalAlignment(JLabel.RIGHT);
+        displayLabel.setText("0");
+        displayLabel.setOpaque(true);
+
+        displayPanel.setLayout(new BorderLayout());
+        displayPanel.add(displayLabel);
+        frame.add(displayLabel, BorderLayout.NORTH);
     }
 }
